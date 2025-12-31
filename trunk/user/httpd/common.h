908ded85401855ae00fafd0de1ad71f89e8eb0f4
haoxinyu1 <62529591+haoxinyu1@users.noreply.github.com>
2025-12-31 11:03:06 +0800

    Update common.h

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -88,7 +88,8 @@ typedef u_int8_t u8;
 // #define EVM_RESTART_ITUNES			(1ULL << 40) //Firefly iTunes 媒体服务器
 #define EVM_RESTART_SS_TUNNEL			(1ULL << 39) //科学上网插件
 //#define EVM_RESTART_TRMD			(1ULL << 40) //#TRANSMISSION
-#define EVM_RESTART_DDNSTO	    		(1ULL << 40) //ddnsto内网穿透
+#define EVM_RESTART_ALIDDNS			(1ULL << 40) //阿里ddns
+// #define EVM_RESTART_DDNSTO	    		(1ULL << 40) //ddnsto内网穿透
 #define EVM_RESTART_FRP	    			(1ULL << 41) //frp内网穿透
 #define EVM_RESTART_ARIA			(1ULL << 42) //aria2c文件下载
 //#define EVM_RESTART_SCUT			(1ULL << 43) //校园网
@@ -96,7 +97,6 @@ typedef u_int8_t u8;
 //#define EVM_RESTART_VLMCSD			(1ULL << 44) //微软服务
 #define EVM_RESTART_ALIST			(1ULL << 44) //alist文件列表
 #define EVM_RESTART_SHADOWSOCKS		(1ULL << 45) //科学上网ss
-// #define EVM_RESTART_ALIDDNS			(1ULL << 45) //阿里ddns
 #define EVM_RESTART_SMARTDNS	    		(1ULL << 46) //smartdns加速
 // #define EVM_RESTART_DNSFORWARDER		(1ULL << 47) //dns转发
 #define EVM_RESTART_CLOUDFLARE			(1ULL << 47)  //CF的ddns
